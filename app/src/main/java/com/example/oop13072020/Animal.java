c955fcc7267f88b5df1917a8be7421afead3ccff
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-10 19:10:06 +0700

    tinh dong goi

--- app/src/main/java/com/example/oop13072020/Animal.java
+++ app/src/main/java/com/example/oop13072020/Animal.java
@@ -3,9 +3,9 @@ package com.example.oop13072020;
 import android.util.Log;
 
 public class Animal {
-    String category;
-    int height;
-    int weight;
+    private String category;
+    private int height;
+    private int weight;
 
     public void eat(Food food){
         switch (food){
@@ -16,5 +16,29 @@ public class Animal {
                 Log.d("BBB","herbivore");
                 break;
         }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
